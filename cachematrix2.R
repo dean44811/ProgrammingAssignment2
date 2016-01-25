ca72d9e75a1f3d9e2e2dcaa417f24671c56b2550
dean44811 <charlie44811@gmail.com>
2016-01-24 20:35:48 -0500

    Rename cachematrix.R to cachematrix2.R

---
+++ cachematrix2.R
@@ -0,0 +1,31 @@
+## makeCatchMatrix captures matrix and puts the inverse of the matrix 
+## in a different environment
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+## chachSolve reads the inverted matrix from that environment
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
